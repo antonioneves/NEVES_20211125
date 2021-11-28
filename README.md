c361beaaaf67b42958fb310ed6c00598e2e80402
antonioneves <antonio.neves.6@gmail.com>
2021-11-28 13:48:41 +0100

    Update to run both server and client

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 Go into *api* folder `cd api`
 
+Install npm dependencies: `npm install`
+
 Run docker compose with Postgresql: `npm run start-db`
 
 Start server: `npm start`
-
-If it is the first time you are running the application, please seed the categories table: `npm run seed-db`
 
 ***
 
@@ -14,4 +14,20 @@ If it is the first time you are running the application, please seed the categor
 
 Go into *client* folder `cd client`
 
+Install npm dependencies: `npm install`
+
 Start server: `npm start`
+
+The start page is on [http://localhost:3000](http://localhost:3000)
+
+***
+
+**Run both**
+
+Install npm dependencies: `npm install`
+
+Start Postgresql database: `npm run start-db`
+
+Start server: `npm start`
+
+Go to [http://localhost:3000](http://localhost:3000)
--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routers = require("./routers");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const { exec } = require("child_process");
 
 const { sequelize } = require("./models");
 
@@ -11,15 +12,22 @@ app.use([
   express.json(),
   cors(),
   fileUpload({
-    limits: { 
-      fileSize: 200 * 1024 * 1024 // 200MB
+    limits: {
+      fileSize: 200 * 1024 * 1024, // 200MB
     },
-    createParentPath: true
+    createParentPath: true,
   }),
 ]);
 app.use("/", routers);
 
 sequelize.sync({ force: false }).then(() => {
+  exec("npm run seed-db", (err, stdout, stderr) => {
+    if (err)
+      return console.log("Seeds already applied");
+    if (stderr)
+      return console.log(`stderr: ${stderr}`);
+    console.log(`stdout: ${stdout}`);
+  });
   app.listen(3000, () => {
     console.log("server is running on port 3000");
   });
